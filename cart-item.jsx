96b88d75e51727489ff6eac64ed4e045886dae79
smitd7 <117810432+smitd7@users.noreply.github.com>
2023-03-20 17:04:42 +0530

    Add files via upload

---
+++ cart-item.jsx
@@ -0,0 +1,27 @@
+import React,{useContext} from 'react'
+import Product from './product'
+import { ShopContext } from '../context/shop-context'
+
+const CartItem = (props) => {
+    const {id,title,price,productImage}=props.data
+    const {cartItems,addToCart,removeFromCart,updateCartItemCount}=useContext(ShopContext)
+  return (
+    <div className='cartItem'>
+        <img src={productImage} />
+        <div className='description'>
+            <p>
+                <b>{title}</b>
+            </p>
+            <p>${price}</p>
+            <div className='countHandler'>
+                <button onClick={()=>removeFromCart(id)}>-</button>
+                <input value={cartItems[id]} onChange={(e)=>updateCartItemCount(Number(e.target.value),id)}/>
+                <button onClick={()=>addToCart(id)}>+</button>
+            </div>
+        </div>
+      
+    </div>
+  )
+}
+
+export default CartItem
--- product.jsx
+++ product.jsx
@@ -1,7 +1,10 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { ShopContext } from '../context/shop-context';
 
 const Product = (props) => {
     const {id,title,price,productImage}=props.data;
+    const {addToCart,cartItems}=useContext(ShopContext)
+    const cartItemAmount=cartItems[id]
   return (
     <div className='product'> 
         <img src={productImage} />
@@ -11,7 +14,7 @@ const Product = (props) => {
             </p>
             <p>${price}</p> 
         </div> 
-        <button className='addToCartBttn'>Add To Cart</button>
+        <button className='addToCartBttn' onClick={()=>addToCart(id)}>Add To Cart {cartItemAmount>0 && <>({cartItemAmount})</>}</button>
     </div>
   ) 
 }
